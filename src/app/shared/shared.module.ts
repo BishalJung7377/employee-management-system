40bae35d142774d60404a23a46880a6d6abdb548
BishalJung <bishaljung63@gmail.com>
2022-10-13 22:50:33 +0545

    Shared module is created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule, 
+  ],
+  declarations: [
+  ],
+  exports: [
+    CommonModule,
+  ],
+  providers: [
+
+  ]
+})
+export class SharedModule { }
